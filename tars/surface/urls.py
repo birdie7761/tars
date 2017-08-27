518f6ab778417cd1b711f467cdf88368fc3f6426
黎勇 <liyong@souche.com>
2017-08-27 19:50:58 +0800

    fix url regex error

--- tars/surface/urls.py
+++ tars/surface/urls.py
@@ -4,7 +4,9 @@ from tars.surface import views
 
 
 urlpatterns = [
-        url(r'^$', views.index, name='surface_index'),
-        url(r'^ping/$', views.ping, name='surface_ping'),
-        url(r'^settings/$', views.export_settings, name='settings'),
-        ]
+    url(r'^$', views.index, name='surface_index'),
+    url(r'^ping/$', views.ping, name='surface_ping'),
+    url(r'^settings/$', views.export_settings, name='settings'),
+    url(r'^login/$', views.login),
+    url(r'^logout/$', views.logout),
+]
--- tars/urls.py
+++ tars/urls.py
@@ -8,11 +8,10 @@ import tars
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^api/v1/', include('tars.api.urls')),
-    url(r'^(surface/)?', include("tars.surface.urls")),
     url(r'^domaininfo/', TemplateView.as_view(template_name='OnService.html')), # health check flag
     url(r'^favicon\.ico$', RedirectView.as_view(url='/static/logo.ico')),
-    url(r'^login/$', tars.surface.views.login),
-    url(r'^logout/$', tars.surface.views.logout),
+    url(r'^surface/', include("tars.surface.urls")),
+    url(r'^', include("tars.surface.urls")),
 ]
 
 # if settings.DEBUG:
